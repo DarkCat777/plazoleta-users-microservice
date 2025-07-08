624d595c112d51ec99ba5747a275c8d3ef396018
Erick David Carpio Hachiri <kuro_coder@MacBook-Pro-de-Erick.local>
2025-07-08 02:13:55 -0500

    Initial commit

---
+++ src/main/java/com/pragma/users/UsersApplication.java
@@ -0,0 +1,24 @@
+package com.pragma.users;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(info = @Info(title = "San Felipe Dental Care", version = "1.0"))
+@SecurityScheme(
+        name = "Bearer Auth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+@SpringBootApplication
+public class UsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersApplication.class, args);
+    }
+
+}
